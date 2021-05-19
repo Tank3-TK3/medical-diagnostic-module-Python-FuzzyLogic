eb5e4a435e3036077394a44bff669cbb3bf06e47
Roberto Cruz Lozano <rcruz15@alumnos.uaq.mx>
2021-05-19 17:50:56 -0500

    Update file

--- main1.py
+++ main1.py
@@ -1,6 +1,5 @@
 ###################################################################################################
 #                                            <MODULES>
-from os import name
 import numpy as np
 import random
 ###################################################################################################
@@ -9,16 +8,16 @@ class MedicalDiagnosticModule():
     def __init__( self ):
         self.preloadedmatrix = np.array( 
             [
-        [ 0.5 ,	0   , 0.9 , 0.8 , 0.8 , 0   , 0   , 0.6 , 0   , 0   , 0   , 0.4 , 0   , 0   , 0   ] ,
-        [ 0.6 ,	0   , 0   , 0   , 0.5 , 0.7 , 0   , 0   , 0   , 0   , 0   , 0   , 0.7 , 0   , 0   ] ,
-        [ 0.9 ,	0.3 , 0.8 , 0.8 , 0   , 0   , 0.5 , 0.6 , 0   , 0   , 0   , 0.7 , 0   , 0.5 , 0   ] ,
-        [ 0.9 ,	0.7 , 0.7 , 0   , 0   , 0   , 0.6 , 0.9 , 0   , 0   , 0   , 0   , 0   , 0   , 0   ] ,
-        [ 0   , 0   , 0.9 , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0.9 , 0   , 0   , 0   ] ,
-        [ 0   , 0   , 0.3 , 0   , 0.5 , 0   , 0   , 0   , 0   , 0   , 0.8 , 0.7 , 0   , 0   , 0   ] ,
-        [ 0.4 , 0.7 , 0.9 , 0.6 , 0   , 0   , 0.6 , 0   , 0   , 0   , 0   , 0.3 , 0.5 , 0   , 0   ] ,
-        [ 0.3 , 0   , 0.7 , 0.8 , 0   , 0   , 0.5 , 0   , 0   , 0   , 0.6 , 0.9 , 0   , 0   , 0.4 ] ,
-        [ 0   , 0   , 0.8 , 0.7 , 0   , 0   , 0   , 0   , 0   , 0.7 , 0.5 , 0.6 , 0   , 0   , 0.6 ] ,
-        [ 0   , 0   , 0.5 , 0.5 , 0.3 , 0   , 0.7 , 0   , 0.9 , 0.4 , 0.7 , 0.7 , 0   , 0.9 , 0   ]
+                [0.5, 0  , 0.9, 0.8, 0.8, 0  , 0  , 0.6, 0  , 0  , 0  , 0.4, 0  , 0  , 0  ],
+                [0.6, 0  , 0  , 0  , 0.5, 0.7, 0  , 0  , 0  , 0  , 0  , 0  , 0.7, 0  , 0  ],
+                [0.9, 0.3, 0.8, 0.8, 0  , 0  , 0.5, 0.6, 0  , 0  , 0  , 0.7, 0  , 0.5, 0  ],
+                [0.9, 0.7, 0.7, 0  , 0  , 0  , 0.6, 0.9, 0  , 0  , 0  , 0  , 0  , 0  , 0  ],
+                [0  , 0  , 0.9, 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0.9, 0  , 0  , 0  ],
+                [0  , 0  , 0.3, 0  , 0.5, 0  , 0  , 0  , 0  , 0  , 0.8, 0.7, 0  , 0  , 0  ],
+                [0.4, 0.7, 0.9, 0.6, 0  , 0  , 0.6, 0  , 0  , 0  , 0  , 0.3, 0.5, 0  , 0  ],
+                [0.3, 0  , 0.7, 0.8, 0  , 0  , 0.5, 0  , 0  , 0  , 0.6, 0.9, 0  , 0  , 0.4],
+                [0  , 0  , 0.8, 0.7, 0  , 0  , 0  , 0  , 0  , 0.7, 0.5, 0.6, 0  , 0  , 0.6],
+                [0  , 0  , 0.5, 0.5, 0.3, 0  , 0.7, 0  , 0.9, 0.4, 0.7, 0.7, 0  , 0.9, 0  ]
             ] , dtype=np.float64 )
         self.intersectionmatrix = np.zeros( ( 10 , 16 ) , dtype=np.float64 )
         self.usersymptoms = np.zeros( ( 1 , 15 ) , dtype=np.float64 )
@@ -26,7 +25,7 @@ class MedicalDiagnosticModule():
 
     def generateSymptoms( self ):
         for i in range( 0 , len( self.usersymptoms[0] ) ):
-            self.usersymptoms[0][i] = random.randint( 0 , 100 ) / 100
+            self.usersymptoms[0][i] = random.randint( 0 , 100000 ) / 100000
     
     def intersectMatrix( self ):
         for i in range( 0 , self.preloadedmatrix.shape[0] ):
@@ -57,6 +56,6 @@ if __name__ == '__main__':
     mdm = MedicalDiagnosticModule()
     mdm.generateSymptoms()
     mdm.intersectMatrix()
-    mdm.generateCSV( mdm.preloadedmatrix )
-    mdm.generateCSV( mdm.usersymptoms )
-    mdm.generateCSV( mdm.intersectionmatrix )
+    #mdm.generateCSV( mdm.preloadedmatrix )
+    #mdm.generateCSV( mdm.usersymptoms )
+    #mdm.generateCSV( mdm.intersectionmatrix )
