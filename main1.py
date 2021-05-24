99932fb878b425cf2e1f0decc21da14f2c75d7ac
Roberto Cruz Lozano <rcruz15@alumnos.uaq.mx>
2021-05-24 00:06:46 -0500

    Update main1

--- main1.py
+++ main1.py
@@ -1,7 +1,8 @@
 ###################################################################################################
 #                                            <MODULES>
 import numpy as np
-import random
+import json
+import sys
 ###################################################################################################
 #                                             <CLASS>
 class MedicalDiagnosticModule():
@@ -20,13 +21,21 @@ class MedicalDiagnosticModule():
                 [0  , 0.6, 0.9, 0  , 0.6, 0  , 0  , 0.7, 0.9, 0.9, 0  , 0.8, 0  , 0  , 0  ]
             ] , dtype=np.float64 )
         self.intersectionmatrix = np.zeros( ( 10 , 16 ) , dtype=np.float64 )
-        self.usersymptoms = np.zeros( ( 1 , 15 ) , dtype=np.float64 )
+        self.usersymptoms = []
+        self.listdiseases = []
+        self.diagnosisresult = np.zeros( ( 1 , 10 ) , dtype=np.float64 )
         self.indexdoc = 0
 
-    def generateSymptoms( self ):
-        for i in range( 0 , len( self.usersymptoms[0] ) ):
-            self.usersymptoms[0][i] = random.random()
-    
+    def processARGV( self , matrix ):
+        matrix = sys.argv[1]
+        matrix = matrix.replace( '[[' , '' )
+        matrix = matrix.replace( ']]' , '' )
+        matrix = matrix.split( '],[' )
+        self.usersymptoms = np.array( [matrix[0].split( ',' )] , dtype=np.float64 )
+        self.listdiseases = np.array( matrix[1].split( ',' ) )
+        print( self.usersymptoms )
+        print( self.listdiseases )
+
     def intersectMatrix( self ):
         for i in range( 0 , self.preloadedmatrix.shape[0] ):
             for j in range( 0 , self.preloadedmatrix.shape[1] ):
@@ -37,9 +46,11 @@ class MedicalDiagnosticModule():
         for i in range( 0 , self.intersectionmatrix.shape[0] ):
             for j in range( 0 , self.intersectionmatrix.shape[1]-1 ):
                 self.intersectionmatrix[i][15] += self.intersectionmatrix[i][j]
+            self.diagnosisresult[0][i] = self.intersectionmatrix[i][15]
+        return json.dumps(self.diagnosisresult[0].tolist())
 ###################################################################################################
 #                                             <MAIN>
 if __name__ == '__main__':
     mdm = MedicalDiagnosticModule()
-    mdm.generateSymptoms()
-    mdm.intersectMatrix()
+    mdm.processARGV( sys.argv[1] )
+    print(mdm.intersectMatrix())
