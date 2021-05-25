3a8c00c725b7867e7a89027b54f23df9186e84e1
Roberto Cruz Lozano <rcruz15@alumnos.uaq.mx>
2021-05-25 12:56:53 -0500

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,9 +1,4 @@
 ###################################################################################################
-#                                            <MODULES>
-import sys
-import json
-import numpy as np
-###################################################################################################
 #                                             <CLASS>
 class MedicalDiagnosticModule():
     def __init__( self ):
@@ -21,24 +16,26 @@ class MedicalDiagnosticModule():
                 [0  , 0.6, 0.9, 0  , 0.6, 0  , 0  , 0.7, 0.9, 0.9, 0  , 0.8, 0  , 0  , 0  ]
             ] , dtype=np.float64 )
         self.intersectionmatrix = np.zeros( ( 10 , 16 ) , dtype=np.float64 )
+        self.diagnosisresult = np.zeros( ( 1 , 10 ) , dtype=np.float64 )
         self.usersymptoms = []
         self.listdiseases = []
-        self.diagnosisresult = np.zeros( ( 1 , 10 ) , dtype=np.float64 )
         self.indexdoc = 0
 
     def processARGV( self , matrix ):
         matrix = sys.argv[1]
+        matrix = matrix.replace( ' ' , '' )
+        matrix = matrix.replace( ' ' , '' )
         matrix = matrix.replace( '[[' , '' )
         matrix = matrix.replace( ']]' , '' )
         matrix = matrix.split( '],[' )
         try:
             self.usersymptoms = np.array( [matrix[0].split( ',' )] , dtype=np.float64 )
         except ValueError:
-            print( '>VALUES CANNOT BE CONVERTED TO FLOATS<' )
+            print( '>VALUES CANNOT BE CONVERTED< (Symptom values are incorrect)' )
             exit()
         self.listdiseases = np.array( matrix[1].split( ',' ) )
 
-    def generatesDiagnosis( self ):
+    def generalDiagnosis( self ):
         for i in range( 0 , self.preloadedmatrix.shape[0] ):
             for j in range( 0 , self.preloadedmatrix.shape[1] ):
                 if self.preloadedmatrix[i][j] >= self.usersymptoms[0][j]:
@@ -50,6 +47,9 @@ class MedicalDiagnosticModule():
                 self.intersectionmatrix[i][15] += self.intersectionmatrix[i][j]
             self.diagnosisresult[0][i] = self.intersectionmatrix[i][15]
         return json.dumps(self.diagnosisresult[0].tolist())
+
+    def specificDiagnosis( self ):
+        return json.dumps(self.diagnosisresult[0].tolist())
 ###################################################################################################
 #                                             <MAIN>
 if __name__ == '__main__':
@@ -60,8 +60,11 @@ if __name__ == '__main__':
     except IndexError:
         print( '>ARGUMENTS NOT RECEIVED<' )
         exit()
-    try:
-        print( mdm.generatesDiagnosis() )
-    except IndexError:
-        print( '>INVALID ARGUMENTS RECEIVED<' )
-        exit()
+    if mdm.listdiseases[0] == '':
+        try:
+            print( mdm.generalDiagnosis() )
+        except IndexError:
+            print( '>INVALID ARGUMENTS RECEIVED<' )
+            exit()
+    else:
+        print( mdm.specificDiagnosis() )
